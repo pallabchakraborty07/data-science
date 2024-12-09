ae7c33be22e5fb0f5df7668171327d5e6780bf8c
pallabchakraborty07 <chpallab07@gmail.com>
2024-12-09 22:45:45 +0530

    Add files via upload

---
+++ L1A1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a1 = np.array([1,2,3,4,5])
+a2 = np.array([6,7,8,9,10])
+
+print(a1+a2)
+print(a1-a2)
+print(a1*a2)
+print(a1/a2)
+
+print(np.sin(a1))
+print(np.cos(a2))
+
+print(a1[2])
+
+print(a1[0:4])
+
+print(np.mean(a1))
+print(np.median(a2))
+print(np.max(a2))
