644dafaa0a54ad76114afae6e19a38cab552c952
pallabchakraborty07 <chpallab07@gmail.com>
2025-02-17 22:32:06 +0530

    Add files via upload

---
+++ rules.py
@@ -0,0 +1,15 @@
+# A = {1,2,3,4,5,6}
+# B = {3,4,5,6}
+
+# C = A.union(B)
+# D = A.intersection(B)
+
+# print(C)
+# print(D)
+
+G_list = {"anik","ankan","pallab","sumon","pritam"}
+I_list = {"pallab","sumon","pritam"}
+
+print(len(G_list))
+print(len(I_list))
+print(len(G_list)-len(G_list.intersection(I_list)))
